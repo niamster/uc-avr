1a960df63b33f087aedfa33d16a7ccbf855ac274
Dmytro Milinevskyy <milinevskyy@gmail.com>
2011-08-14 03:21:32 +0300

    [ATmega128][GCC][with-alnum-lcd-driver] async LCD driver

--- stk128/AVR-ATmega128-GCC-with-alnum-lcd-driver/main.c
+++ stk128/AVR-ATmega128-GCC-with-alnum-lcd-driver/main.c
@@ -7,6 +7,20 @@
 typedef unsigned char u8;
 typedef signed char s8;
 
+u8 irq_save(void)
+{
+    // Save status register and disable interrupts
+    u8 flags = SREG;
+    cli();
+    return flags;
+}
+
+void irq_restore(u8 flags)
+{
+    // Re-enable interrupts (if they were ever enabled)
+    SREG = flags;
+}
+
 #define configure_leds(port) do {               \
         DDR##port = 0xFF;                       \
     } while (0)
@@ -18,54 +32,170 @@ typedef signed char s8;
 /* PORTA[0] = busy */
 /* PORTA[1] = clk */
 /* PORTA[2] = data */
+
 void lcd_init(void)
 {
     DDRA |= 0x6;
     DDRA &= ~0x1;
 
     PORTA &= ~0x6;
+
+    /* OC1A/OC1B disconnected */
+    TCCR1A = 0;
+#if 0
+    /* CTC mode, top in OCR1A / Timer clock = CLK/1024 */
+    TCCR1B = (1<<WGM12)|(1<<CS12);
+    /* set OCR1A top value for 7KHz (7000/2 periods in one second) */
+    /*   0x01 for CPU @ 7.3728MHz (7372800/1024)/7000 */
+    OCR1A = 0x01;
+#else
+    /* CTC mode, top in OCR1A / Timer clock = CLK */
+    TCCR1B = (1<<WGM12);
+    /* set OCR1A top value for 1MHz (1,000,000/2 periods in one second) */
+    /*   0x1CCD for CPU @ 7.3728MHz 7372800/1000 */
+    OCR1A = 0x1CCD;
+#endif
+    /* enable Timer/Counter 1 interrupt */
+    TIMSK |= (1<<OCIE1A);
 }
 
-void lcd_putb(u8 c)
+#define LCD_CMD_FIFO_LEN 256
+#define LCD_CMD_FIFO_MSK (LCD_CMD_FIFO_LEN - 1)
+
+u8 lcd_cmd_fifo[LCD_CMD_FIFO_LEN];
+volatile u8 lcd_cmd_fifo_rd = 0, lcd_cmd_fifo_wr = 0;
+
+void lcd_process_cmd_nolock(u8 max)
 {
+    u8 cmd, curr;
     s8 i;
 
-    c |= 1<<7; /* mandatory bit */
+    if (!max)
+        return;
 
-    while (PINA&1);
+    while (max--) {
+        if (lcd_cmd_fifo_rd == lcd_cmd_fifo_wr) {
+            /* Disable timer */
+            TCCR1B &= ~(1<<CS10);
+            return;
+        }
 
-    for (i=7;i>=0;--i) {
-        if ((c>>i)&0x1)
-            PORTA |= 1<<2;
-        else
-            PORTA &= ~(1<<2);
+        if (PINA&1)
+            return;
 
-        PORTA |= 1<<1;
-        PORTA &= ~(1<<1);
+        /* ASSERT(lcd_cmd_fifo_rd != lcd_cmd_fifo_wr); */
+
+        curr = lcd_cmd_fifo_rd;
+        cmd = lcd_cmd_fifo[curr];
+        lcd_cmd_fifo_rd = (curr + 1)&LCD_CMD_FIFO_MSK;
+
+        for (i=7;i>=0;--i) {
+            if ((cmd>>i)&0x1)
+                PORTA |= 1<<2;
+            else
+                PORTA &= ~(1<<2);
+
+            PORTA |= 1<<1;
+            PORTA &= ~(1<<1);
+        }
     }
 }
 
-#define lcd_clr() lcd_putb(0x61)
-#define lcd_ver() lcd_putb(0x60)
-#define lcd_goto(row, col) do { lcd_putb(0x40|(row)); lcd_putb(col); } while (0)
+void lcd_process_cmd(u8 max)
+{
+    u8 cmd, curr;
+    s8 i;
+    u8 flags;
+
+    flags = irq_save();
+    lcd_process_cmd_nolock(max);
+    irq_restore(flags);
+}
+
+ISR(TIMER1_COMPA_vect, ISR_BLOCK)
+{
+    lcd_process_cmd_nolock((u8)~0);
+}
+
+void lcd_push_cmd_nolock(u8 c)
+{
+    u8 curr, next;
+
+    c |= 1<<7; /* mandatory bit */
+
+    do {
+        lcd_process_cmd_nolock(1);      /* move the queue to avoid deadlock */
+
+        curr = lcd_cmd_fifo_wr;
+        next = (curr + 1)&LCD_CMD_FIFO_MSK;
+    } while (next == lcd_cmd_fifo_rd);
+
+    lcd_cmd_fifo[curr] = c;
+
+    lcd_cmd_fifo_wr = next;
+
+    /* Enable timer */
+    TCCR1B |= (1<<CS10);
+}
+
+#define lcd_clr() do { u8 flags = irq_save(); lcd_push_cmd_nolock(0x61); irq_restore(flags); } while (0)
+#define lcd_ver() do { u8 flags = irq_save(); lcd_push_cmd_nolock(0x60); irq_restore(flags); } while (0)
+#define lcd_goto_nolock(row, col) do { lcd_push_cmd_nolock(0x40|(row)); lcd_push_cmd_nolock(col); } while (0)
+#define lcd_goto(row, col) do { u8 flags = irq_save(); lcd_goto_nolock(row, col); irq_restore(flags); } while (0)
+
+void lcd_putc_nolock(u8 c)
+{
+    lcd_push_cmd_nolock(1);
+    lcd_push_cmd_nolock(c);
+}
 
 void lcd_putc(u8 c)
 {
-    lcd_putb(1);
-    lcd_putb(c);
+    u8 flags = irq_save();
+
+    lcd_putc_nolock(c);
+
+    irq_restore(flags);
 }
 
 void lcd_puts(u8 *s)
 {
+    u8 flags;
+
     if (!s || !*s)
         return;
 
-    lcd_putb(strlen(s)&0x1F);
+    flags = irq_save();
+
+    lcd_push_cmd_nolock(strlen(s)&0x1F);
 
     while (*s) {
-        lcd_putb(*s);
+        lcd_push_cmd_nolock(*s);
         ++s;
     }
+
+    irq_restore(flags);
+}
+
+u8 chars[] = "Oo";
+u8 cur;
+
+ISR(TIMER3_COMPA_vect, ISR_NOBLOCK)
+{
+    u8 flags;
+
+    flags = irq_save();
+    lcd_goto_nolock(3, 0);
+    lcd_putc_nolock(chars[cur]);
+    irq_restore(flags);
+
+    flags = irq_save();
+    lcd_goto_nolock(3, 19);
+    lcd_putc_nolock(chars[cur]);
+    irq_restore(flags);
+
+    ++cur;
+    cur &= sizeof(chars) - 2;
 }
 
 int main(void)
@@ -75,7 +205,20 @@ int main(void)
 
     lcd_init();
 
+    /* enable interrupts */
+    sei();
+
     lcd_clr();
+
+    /* OC3A/OC3B disconnected */
+    TCCR3A = 0;
+    /* CTC mode, top in OCR3A / Timer clock = CLK/1024 */
+    TCCR3B = (1<<WGM32)|(1<<CS32)|(1<<CS30);
+    /* set OCR3A top value for 2KHz (2/2 periods in one second) */
+    /*   0x01 for CPU @ 7.3728MHz (7372800/1024)/2 */
+    OCR3A = 0x0E10;
+    /* enable Timer/Counter 3 interrupt */
+    ETIMSK |= (1<<OCIE3A);
 
     lcd_goto(0, 0);
     lcd_ver();
